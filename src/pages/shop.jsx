65ed68999fb9dbddd2b90db4149b187f21c8a4aa
mohamed-x-19 <mohamedahmed137713@gmail.com>
2026-09-23 16:35:21 +0300

    update

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -209,8 +209,8 @@ const Shop = () => {
           </div>
         ) : (
           <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {filteredProducts.map((product) => (
-              <ProductCard product={product} />
+            {filteredProducts.map((product, index) => (
+              <ProductCard key={index} product={product} />
             ))}
           </div>
         )}
